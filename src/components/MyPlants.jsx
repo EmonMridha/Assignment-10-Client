9f8078f87e21eddb1925d3610f8ff5a811fa4f57
EmonMridha <emonmridha712@gmail.com>
2025-10-18 17:42:28 +0600

    added myplants

--- src/components/MyPlants.jsx
+++ src/components/MyPlants.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Context/AuthContext";
+
 
 const MyPlants = () => {
+
+    const { user } = useContext(AuthContext);
+    const [plants, setPlants] = useState([])
+
+    useEffect(() => {
+        if (user?.email) {
+            fetch(`http://localhost:3000/userPlants/${user.email}`)
+                .then(res => res.json())
+                .then(data => {
+                    setPlants(data);
+                    console.log(data);
+                })
+        }
+    },[user])
+
+    
+
+
     return (
         <div>
-            My Plants
+
         </div>
     );
 };
